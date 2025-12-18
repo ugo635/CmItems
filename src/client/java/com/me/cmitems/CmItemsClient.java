caeeea864004790630ef9d1181e4b077d8bc0991
ugo635 <ugolechien58901@gmail.com>
2025-12-18 14:51:25 +0100

    Main

---
+++ src/client/java/com/me/cmitems/CmItemsClient.java
@@ -0,0 +1,10 @@
+package com.me.cmitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CmItemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
