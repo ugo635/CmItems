f15c74bb5aef6267311f70d969a33bc18c64201f
ugo635 <ugolechien58901@gmail.com>
2025-12-20 16:58:19 +0100

    Fix

--- src/main/java/com/me/cmitems/CmItems.java
+++ src/main/java/com/me/cmitems/CmItems.java
@@ -4,14 +4,14 @@ import com.me.cmitems.items.EnderBow;
 import com.me.cmitems.items.GravityBow;
 import com.me.cmitems.items.LightningStick;
 import com.me.cmitems.items.TntBow;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CmItems implements ModInitializer {
 	public static final String MOD_ID = "cmitems";
-	public static MinecraftClient mc = MinecraftClient.getInstance();
+	//public static MinecraftClient mc = MinecraftClient.getInstance();
 
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -25,4 +25,5 @@ public class CmItems implements ModInitializer {
 
 		CustomCreativeItem.register();
 	}
+
 }
