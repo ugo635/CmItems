cda74755567e264de36efcd06a01075a9db57843
ugo635 <ugolechien58901@gmail.com>
2026-03-14 22:22:20 +0100

    Refactored the drill's code to use a parent class
    Added an overlay to all blocks in the drill's area
    
    Made the drill work in creative

--- src/main/java/com/me/cmitems/items/tools/drills/DiamondDrill.java
+++ src/main/java/com/me/cmitems/items/tools/drills/DiamondDrill.java
@@ -3,27 +3,11 @@ package com.me.cmitems.items.tools.drills;
 import com.me.cmitems.creator.ModItems;
 import com.me.cmitems.toolmaterial.DrillMaterial;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.fabricmc.fabric.api.item.v1.EnchantingContext;
-import net.minecraft.block.BlockState;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.ToolComponent;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.World;
-
-import java.util.List;
 
 // FIXME: Enchantments can't be applied through enchantment table, but can be applied through anvil.
-public class DiamondDrill extends Item {
+public class DiamondDrill extends GeneralDrill {
     public static Item DIAMOND_DRILL = ModItems.register(
             "diamond_drill",
             DiamondDrill::new,
@@ -31,8 +15,6 @@ public class DiamondDrill extends Item {
                     .pickaxe(DrillMaterial.DIAMOND_DRILL_MATERIAL, 1.0f, -2.8f)
                     .maxCount(1)
     );
-
-    public static Direction blockFace;
 
     public DiamondDrill(Settings settings) {
         super(settings);
@@ -45,81 +27,6 @@ public class DiamondDrill extends Item {
                 list.add(1, Text.translatable("itemTooltip.cmitems.diamond_drill"));
             }
         });
-
-        // When start mining, set the direction to the face of the block being mined. This is used to determine the 3x3 area to mine.
-        AttackBlockCallback.EVENT.register((player, world, hand, blockPos, direction) -> {
-            blockFace = direction;
-            return ActionResult.PASS;
-        });
-    }
-
-    @Override
-    public boolean canBeEnchantedWith(ItemStack stack, RegistryEntry<Enchantment> enchantment, EnchantingContext context) {
-        return super.canBeEnchantedWith(stack, enchantment, context);
-    }
-
-    @Override
-    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        ToolComponent toolComponent = stack.get(DataComponentTypes.TOOL);
-        if (toolComponent == null) {
-            return false;
-        } else {
-            if (!world.isClient && state.getHardness(world, pos) != 0.0f && toolComponent.damagePerBlock() > 0) {
-                stack.damage(toolComponent.damagePerBlock(), miner, EquipmentSlot.MAINHAND);
-                mine3x3(stack, world, pos, miner);
-            }
-
-            return true;
-        }
-    }
-
-    private void mine3x3(ItemStack stack, World world, BlockPos centerPos, LivingEntity miner) {
-        List<BlockPos> blocksToMine = get3x3BlockPositions(centerPos);
-
-        for (BlockPos pos : blocksToMine) {
-            BlockState state = world.getBlockState(pos);
-            if (state.getHardness(world, pos) != 0.0f) {
-                world.breakBlock(pos, true, miner);
-            }
-        }
-    }
-
-    private List<BlockPos> get3x3BlockPositions(BlockPos centerPos) {
-        return switch (blockFace) {
-            case UP, DOWN -> List.of(
-                    centerPos.north().west(),
-                    centerPos.north(),
-                    centerPos.north().east(),
-                    centerPos.west(),
-                    centerPos.east(),
-                    centerPos.south().west(),
-                    centerPos.south(),
-                    centerPos.south().east()
-            );
-
-            case NORTH, SOUTH -> List.of(
-                    centerPos.up().west(),
-                    centerPos.up(),
-                    centerPos.up().east(),
-                    centerPos.west(),
-                    centerPos.east(),
-                    centerPos.down().west(),
-                    centerPos.down(),
-                    centerPos.down().east()
-            );
-
-            case EAST, WEST -> List.of(
-                    centerPos.up().north(),
-                    centerPos.up(),
-                    centerPos.up().south(),
-                    centerPos.north(),
-                    centerPos.south(),
-                    centerPos.down().north(),
-                    centerPos.down(),
-                    centerPos.down().south()
-            );
-
-        };
 
     }
 
---
+++ src/main/java/com/me/cmitems/mixin/BlockOutlineDrillMixin.java
@@ -0,0 +1,91 @@
+package com.me.cmitems.mixin;
+
+import com.me.cmitems.items.tools.drills.GeneralDrill;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.Colors;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(WorldRenderer.class)
+public class BlockOutlineDrillMixin {
+
+    // @Shadow just copy WorldRenderer's fields.
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Shadow
+    private ClientWorld world;
+
+    @Shadow
+    private void drawBlockOutline(MatrixStack matrices, VertexConsumer vertexConsumer, Entity entity, double x, double y, double z,
+                                  BlockPos blockPos, BlockState blockState, int color) {}
+
+    @Inject(method = "renderTargetBlockOutline", at = @At("HEAD"), cancellable = true)
+    private void onRenderOutline(Camera camera, VertexConsumerProvider.Immediate vertexConsumers, MatrixStack matrices, boolean translucent, CallbackInfo ci) {
+        // Default code
+        if (client.player != null && client.player.getMainHandStack().getItem() instanceof GeneralDrill drill) {
+            HitResult var6 = this.client.crosshairTarget;
+            if (var6 instanceof BlockHitResult blockHitResult) {
+                if (blockHitResult.getType() != HitResult.Type.MISS) {
+                    BlockPos blockPos = blockHitResult.getBlockPos();
+                    BlockState blockState = this.world.getBlockState(blockPos);
+                    if (!blockState.isAir() && this.world.getWorldBorder().contains(blockPos)) {
+                        boolean bl = RenderLayers.getBlockLayer(blockState).isTranslucent();
+                        if (bl != translucent) {
+                            return;
+                        }
+
+                        Vec3d vec3d = camera.getPos();
+                        Boolean boolean_ = (Boolean) this.client.options.getHighContrastBlockOutline().getValue();
+                        VertexConsumer vertexConsumer;
+                        if (boolean_) {
+                            vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getSecondaryBlockOutline());
+                            this.drawBlockOutline(matrices, vertexConsumer, camera.getFocusedEntity(), vec3d.x, vec3d.y, vec3d.z, blockPos, blockState, -16777216);
+                            // Adding all new blocks from the drill's 3x3 area
+                            drawDrillAreaOutline(camera, matrices, drill, blockPos, vec3d, vertexConsumer);
+                        }
+
+                        vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getLines());
+                        int i = boolean_ ? Colors.CYAN : ColorHelper.withAlpha(102, Colors.BLACK);
+                        this.drawBlockOutline(matrices, vertexConsumer, camera.getFocusedEntity(), vec3d.x, vec3d.y, vec3d.z, blockPos, blockState, i);
+                        // Adding all new blocks from the drill's 3x3 area
+                        drawDrillAreaOutline(camera, matrices, drill, blockPos, vec3d, vertexConsumer);
+                        vertexConsumers.drawCurrentLayer();
+                    }
+
+                }
+            }
+            ci.cancel();
+        }
+    }
+
+    @Unique
+    private void drawDrillAreaOutline(Camera camera, MatrixStack matrices, GeneralDrill drill, BlockPos blockPos, Vec3d vec3d, VertexConsumer vertexConsumer) {
+        List<BlockPos> positions = drill.getBlockAreaPosition(blockPos);
+        System.out.println("Drill area size: " + positions.size());
+        for (BlockPos pos : positions) {
+            BlockState state = this.world.getBlockState(pos);
+            if (!state.isAir() && this.world.getWorldBorder().contains(pos)) {
+                this.drawBlockOutline(matrices, vertexConsumer, camera.getFocusedEntity(), vec3d.x, vec3d.y, vec3d.z, pos, state, -16777216);
+            }
+        }
+    }
+}
